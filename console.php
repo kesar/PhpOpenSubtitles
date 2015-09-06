1ebad29383a1877d6c13bec2e3411d9f35db708a
Diego <diego@sv2.com.br>
2015-09-06 20:48:18 -0300

    Update console.php
    fixed typo on variable name

--- console.php
+++ console.php
@@ -33,7 +33,7 @@ if (empty($config)) {
     return 1;
 }
 
-$manager   = new SubtitlesManager($config['username'], $config['password'], $config['lang']);
+$manager   = new SubtitlesManager($config['username'], $config['password'], $config['language']);
 $subtitles = $manager->get($file);
 
 if (!empty($subtitles) && !empty($subtitles[0])) {
